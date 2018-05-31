987ce8b90a5067df7d478e37b315fd9e25a056af
YashIsDeep <yashparth.gupta@gmail.com>
2018-05-31 16:57:49 +0530

    Most of the functions finished.May remove Dependencies.

--- Cleaner.py
+++ Cleaner.py
@@ -1,10 +1,13 @@
 import re
-import autocorrect
+import autocorrect # for spell correction
+import wordninja
 
+# Currently supports only English
+# Need to add a way to replace backslash characters
 def replaceMentionsWithNames(text): # Replaces the @names with *Name* and returns the names used
 	namePattern = re.compile(r"@[a-zA-Z0-9_]{1,15}")
 	names=set()
-	modString="" # modStirng stores the modified string ,String will be modified multiple times for different purposes
+	modString="" 
 	matches = re.finditer(namePattern,text)
 	end=0
 	for match in matches:
@@ -14,13 +17,27 @@ def replaceMentionsWithNames(text): # Replaces the @names with *Name* and return
 	modString=modString+text[end:]
 	return modString,names
 
-def deleteEmotes(str):
-	emoji_pattern = re.compile(":(\(\)\\/OPI|\[\])")
+def deleteEmotes(str):  # Needs improvement
+	emoji_pattern = re.compile(r":(\(\)\\/OPI|\[\])")
+	# emoji_pattern = re.compile(r"[^a-zA-Z0-9_ !?.,]") Possible alternative
 	str=emoji_pattern.sub(r"",str)
 	return str
 
-def breakDownHashtags(str):
-	return str
+def breakDownHashtags(text):
+	def splitWordAndAddSpaces(word): # add space in front as well as end    ##DEPENDENCY to remove##
+			wordlist = wordninja.split(word)
+			string = ' '.join(eachword for eachword in wordlist )
+		return " "+string+" "
+	tagPattern= re.compile(r"#[a-zA-Z0-9_]")
+	tags=re.finditer(tagPattern,text)
+	end=0
+	brokenString=""
+	for tag in tags:
+		formattedword=splitWordAndAddSpaces(word)
+		modString=modString+text[end:tag.start()]+changedword
+		end=tag.end()
+	modString=modString+text[end:]
+	return modString
 
 def doSpellCorrection(text):
 	wordPattern = re.compile(r"[^a-zA-Z0-9_][a-zA-Z0-9_]+")
@@ -36,40 +53,22 @@ def doSpellCorrection(text):
 			end=eachword.end()
 			continue
 		correctedword=autocorrect.spell(word)
-		# print(eachword.group(),word,correctedword)
+		# print(eachword.group(),word,correctedword) --log--
 		result=result+text[end:eachword.start()]+eachword.group()[0]+correctedword
 		end=eachword.end()
 	result=result+text[end:]
 	return result
-	# splitPattern=re.compile(r'[^a-zA-Z0-9_]')
-	# words=re.finditer(splitPattern, text) # splits string into words 
-	# result=""
-	# end=0
-	# for eachword in words:
-	# 	word=eachword.group()
-	# 	if len(word)==0:
-	# 		continue
-	# 	if word.upper()=="*NAME*":
-	# 		result=result+text[end:eachword.start()]+word
-	# 		end=eachword.end()
-	# 		continue
-	# 	correctedword=autocorrect.spell(word)
-	# 	result=result+text[end:eachword.start()]+correctedword
-	# 	end=eachword.end()
-	# result=result+text[end:]
-	# return result
 
-# The set 'names' contains all the names now
+def substituteNames(text , names):
+	for eachname in names:
+		text=text.replace(eachname,"*NAME*")
+	return text
 
 inputText=input()
-modString,names=replaceMentionsWithNames(inputText)
+modString,names=replaceMentionsWithNames(inputText) # modStirng stores the modified string ,String will be modified multiple times for different purposes , names stores the list of names mentioned in the text
 modString=deleteEmotes(modString)
 modString=breakDownHashtags(modString)
 modString=doSpellCorrection(" "+modString)[1:]
-
-
+modString=substituteNames(modString)
 
 print(modString)
-
-
-
